bfb5392d013c7b4c3af7519bef5dca4fc9a75cc0
Jose5032 <jr76871922@gmail.com>
2021-05-22 05:08:23 -0600

    My Initial commit

---
+++ NewAcount.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Crear Cuenta</title>
+        <link rel="stylesheet" href="css/NAs.css" />
+    </head>
+    <body>
+        <form method="get">
+            
+        </form>
+    </body>
+</html>
