49d4cf9b2716528c288b0e883931e8cf569627a4
Jose5032 <jr76871922@gmail.com>
2021-05-22 05:06:21 -0600

    My Initial commit

--- upload.sh
+++ upload.sh
@@ -4,4 +4,4 @@ git config --global user.name "Jose5032"
 git config --global usef.email "jr76871922@gmail.com"
 git commit -m " My Initial commit"
 git remote add origin https://github.com/Jose5032/Jose5032.github.io.git
-git push -u origin master
+git push #-u origin master
